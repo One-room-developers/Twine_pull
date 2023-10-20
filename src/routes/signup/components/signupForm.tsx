9b8c6a2d9d48b0c3924a30bd782c3fb2e154dc88
ImJongHoon <imjh990614@gmail.com>
2023-10-20 20:57:14 +0900

    react hook form added

--- src/routes/signup/components/signupForm.tsx
+++ src/routes/signup/components/signupForm.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
-import {Component} from 'react';
 import {Link} from 'react-router-dom'
 import x from '../../../styles/image/x.svg';
 import './signupForm.css';
 import {idCheck, nicknameCheck} from '../../authApi';
-import SessionStorageAPI from '../../login/session';
-import { valid } from 'semver';
+import {useForm} from "react-hook-form";
 
 type SignupForm_props = {
     closeForm() : void,
@@ -13,153 +11,124 @@ type SignupForm_props = {
     emailDefault : string,
 };
 
-type SignupForm_states={
+interface IFormData{
+    error:{
+        id:{
+            message:string;
+        }
+    }
     id:string;
-    idValid:number;
-    nameValid:number;
-    nickname:string;
+    nick:string;
     pwd1:string;
     pwd2:string;
-    pwdValid: number;
 };
 
-class SignupForm extends Component<SignupForm_props, SignupForm_states>{
+function SignupForm (props:SignupForm_props){
 
-    constructor(props){
-        super(props);
-        this.state = {
-            id: this.props.emailDefault,
-            idValid: 0,
-            nameValid: 0,
-            pwdValid: 0,
-            pwd1: "",
-            pwd2: "",
-            nickname: "",
-        };
-        let timeoutId;
-    }
+    const {register, handleSubmit, formState:{errors}, setError} = useForm()
 
-    checkPwdValid(){
-        if(this.state.pwd1 !== this.state.pwd2){
-            this.setState({pwdValid: 1});
-        }
-        else{
-            this.setState({pwdValid: 2});
+    function onValid(data: IFormData){
+        if(data.pwd1 !== data.pwd2){
+            setError("pwd2", {message:"비밀번호가 서로 다릅니다."});
         }
     }
-    
-    
-    render() {
 
-        return(
-            <div className='signup-background'>
-                <form className='signup-info' onSubmit={function(e){
-                    e.preventDefault();
-                    if(this.state.idValid === 0){
-                        alert("아이디 중복 검사를 해주세요.");
-                    }
-                    else if(this.state.nameValid === 0){
-                        alert("닉네임 중복 검사를 해주세요.");
-                    }
-                    else if(this.state.pwdValid !== 2){
-                        alert("비밀번호를 확인해주세요.");
-                    }
-                    else{
-                        this.props.submitForm(e);
-                    }
-                }.bind(this)}>
-                    <div className='signup-header'>
-                        <div></div>
-                        <h1 className='font-game-thick'>
-                            회원가입
-                        </h1>
-                        <div className='signup-close' onClick={function(){this.props.closeForm()}.bind(this)}><img src={x}/></div>
-                    </div>
-                    <div className='signup-title font-game-thick'>
-                        <h1>모험에 도전하시는 여러분, 환영합니다.</h1>
-                    </div>
-                    <div className='signup-line signup-id-line margin-bottom-none font-game-thin'>
-                        <div className='signup-id-container'>
-                            <label htmlFor='email'>아이디</label>
-                            <input type="text" id="email" name='email' value={this.state.id} onChange={
-                                function(e){
-                                    this.setState({id: e.target.value})
-                                }.bind(this)}
-                            required/>
-                            <div onClick={async function(e){
-                                if(await idCheck(this.state.id) === false){
-                                    this.setState({idValid: 1});
-                                }
-                                else{
-                                    this.setState({idValid: 2});
-                                }
-                            }.bind(this)} className='valid-check-btn'>중복검사</div>
-                        </div>
-                        {
-                            this.state.idValid === 0 ?(<h2 className='valid-info'>중복검사를 해주세요</h2>) :
-                            this.state.idValid === 1 ?(<h2 className='valid-info-no'>사용할 수 없는 아이디입니다.</h2>):
-                            this.state.idValid === 2 ?(<h2 className='valid-info-yes'>사용 가능한 아이디입니다.</h2>):(<></>)
-                        }
-                    </div>
-                    
-                    <div className='signup-line  font-game-thin'>
-                        <label htmlFor='pw'>비밀번호</label>
-                        <input type="password" id="pw" name='pw' required onChange={
-                            function(e){
-                                this.setState({pwd1: e.target.value})
-                            }.bind(this)}/>
-                    </div>
+    console.log(errors);
+    return(
+        <div className='signup-background'>
+            <form className='signup-info' onSubmit={handleSubmit(onValid)}>
+                <div className='signup-header'>
+                    <div></div>
+                    <h1 className='font-game-thick'>
+                        회원가입
+                    </h1>
+                    <div className='signup-close' onClick={function(){props.closeForm()}}><img src={x}/></div>
+                </div>
+                <div className='signup-title font-game-thick'>
+                    <h1>모험에 도전하시는 여러분, 환영합니다.</h1>
+                </div>
+                <div className='signup-line signup-id-line font-game-thin'>
+                    <div className='signup-id-container'>
 
-                    <div className='signup-line signup-id-line font-game-thin'>
-                        <div className='signup-id-container'>
-                            <label htmlFor='pwc'>비밀번호 확인</label>
-                            <input type="password" id="pwc" name='pwc' required onChange={
-                                function(e){
-                                    clearTimeout(this.timeoutID);
-                                    this.setState({pwd2: e.target.value});
-                                    this.timeoutID = setTimeout(this.checkPwdValid.bind(this), 300);
-                                }.bind(this)}/>
-                        </div>
-                        {
-                            this.state.pwdValid === 0 ?(<h2 className='valid-info'>비밀번호 확인이 필요합니다.</h2>) :
-                            this.state.pwdValid === 1 ?(<h2 className='valid-info-no'>비밀번호가 일치하지 않습니다.</h2>):
-                            this.state.pwdValid === 2 ?(<h2 className='valid-info-yes'>사용 가능한 비밀번호입니다.</h2>):(<></>)
-                        }
-                    </div>
+                        <label htmlFor='email'>아이디</label>
+                        <input {...register("id", {required: "필수 입력 항목입니다.", 
+                        pattern:{value:/^[A-Za-z0-9._%+-]/,
+                                message: "영문, 숫자로만 조합 가능합니다."},
+                        minLength:{
+                            value: 4,
+                            message:"4-16 글자 사이이어야 합니다."
+                        },
+                        maxLength:{
+                            value: 16,
+                            message:"4-16 글자 사이이어야 합니다."
+                        },
+                        validate:{duplication: async (value) => 
+                                    await idCheck(value) === false ? "이미 사용중인 아이디입니다." : true}})}
+                        type="text" />
 
-                    <div className='signup-line signup-id-line font-game-thin'>
-                        <div className='signup-id-container'>
-                            <label htmlFor='nic'>별명</label>
-                            <input type="text" id="nic" name='nic' value={this.state.nickname} onChange={
-                                function(e){
-                                    this.setState({nickname: e.target.value})
-                                }.bind(this)} required/>
-                            <div onClick={async function(e){
-                                if(await nicknameCheck(this.state.nickname) === false){
-                                    this.setState({nameValid: 1});
-                                }
-                                else{
-                                    this.setState({nameValid: 2});
-                                }
-                            }.bind(this)} className='valid-check-btn'>중복검사</div>
-                        </div>
-                        {
-                            this.state.nameValid === 0 ?(<h2 className='valid-info'>중복검사를 해주세요</h2>) :
-                            this.state.nameValid === 1 ?(<h2 className='valid-info-no'>사용할 수 없는 별명입니다.</h2>):
-                            this.state.nameValid === 2 ?(<h2 className='valid-info-yes'>사용 가능한 별명입니다.</h2>):(<></>)
-                        }
                     </div>
-                    <div className='signup-line font-game-thin input-checkbox'>
-                        {/* <label htmlFor='check'>게임 이용 약관 동의 <a className='terms-a' href='/'> 자세히</a></label> */}
-                        <label htmlFor='check'>베타 기간 이용 안내 <Link className='terms-a' target="_blank" to='/beta'> 자세히</Link></label>
-                        <input type="checkbox" id="check" required/>
+                   <h2 className='valid-info-no'>{errors?.id?.message}</h2>
+                    {/*<h2 className='valid-info-yes'>사용 가능한 아이디입니다.</h2>*/}
+                </div>
+                
+                <div className='signup-line signup-id-line font-game-thin'>
+                    <div className='signup-id-container'>
+                        <label htmlFor='pwd1'>비밀번호</label>
+                        <input {...register("pwd1", {required: "필수 입력 항목입니다.", 
+                        minLength:{
+                            value: 4,
+                            message:"4-16 글자 사이이어야 합니다."
+                        },
+                        maxLength:{
+                            value: 16,
+                            message:"4-16 글자 사이이어야 합니다."
+                        },
+                        })}
+                        type="password"/>
                     </div>
-                    <button className='signup-form-submit-btn'>가입하기</button>
+                    <h2 className='valid-info-no'>{errors?.pwd1?.message}</h2>
+                </div>
 
-                </form>
-            </div>
-        );
-    }
+                <div className='signup-line signup-id-line font-game-thin'>
+                    <div className='signup-id-container'>
+                        <label htmlFor='pwd2'>비밀번호 확인</label>
+                        <input
+                        {...register("pwd2", {required: "필수 입력 항목입니다.", 
+                        })}
+                        type="password" />
+                    </div>
+                    <h2 className='valid-info-no'>{errors?.pwd2?.message}</h2>
+                </div>
+
+                <div className='signup-line signup-id-line font-game-thin'>
+                    <div className='signup-id-container'>
+                        <label htmlFor='nick'>별명</label>
+                        <input {...register("nick", {required: "필수 입력 항목입니다.",
+                        minLength:{
+                            value: 2,
+                            message:"2-16 글자 사이이어야 합니다."
+                        },
+                        maxLength:{
+                            value: 16,
+                            message:"2-16 글자 사이이어야 합니다."
+                        },
+                        validate:{duplication: async (value) => 
+                                    await nicknameCheck(value) === false ? "이미 사용중인 닉네임입니다." : true}})}
+                        type="text"   />
+
+                    </div>
+                    <h2 className='valid-info-no'>{errors?.nick?.message}</h2>
+                </div>
+                <div className='signup-line font-game-thin input-checkbox'>
+                    {/* <label htmlFor='check'>게임 이용 약관 동의 <a className='terms-a' href='/'> 자세히</a></label> */}
+                    <label htmlFor='check'>베타 기간 이용 안내 <Link className='terms-a' target="_blank" to='/beta'> 자세히</Link></label>
+                    <input type="checkbox" id="check" />
+                </div>
+                <button className='signup-form-submit-btn'>가입하기</button>
+
+            </form>
+        </div>
+    );
 }
 
 export default SignupForm;
