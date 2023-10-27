0d3b0cba4a7a42dcdf9119857bf50e1151b7d852
ImJongHoon <imjh990614@gmail.com>
2023-10-27 23:32:01 +0900

    login info and enter

--- src/routes/login/login-route.tsx
+++ src/routes/login/login-route.tsx
@@ -12,10 +12,10 @@ import {userInfoAtom} from "./userInfoAtom";
 export const LoginRoute: React.FC = () => {
 
     const history = useHistory();
-    
+
     const [email, setEmail] = React.useState("");
     const [pwd, setPwd] = React.useState("");
-
+    const [errorInfo, setErrorInfo] = React.useState("");
     //recoil 사용
     const [userNickName, setUserNickName] = useRecoilState(userInfoAtom);
 
@@ -23,8 +23,14 @@ export const LoginRoute: React.FC = () => {
         await setUserNickName(userName);
     }
 
-    const onChangeEmail = React.useCallback((e) => setEmail(e.target.value), []);
-    const onChangePwd = React.useCallback((e) => setPwd(e.target.value), []);
+    const onChangeEmail = React.useCallback((e) => {
+        setErrorInfo("");
+        setEmail(e.target.value);
+    }, []);
+    const onChangePwd = React.useCallback((e) => {
+        setErrorInfo("");
+        setPwd(e.target.value);
+    }, []);
 
     const authorizedUser = {
         email: String,
@@ -32,47 +38,61 @@ export const LoginRoute: React.FC = () => {
     };
 
     function login() {
-        axios({
-            method: "POST",
-            url: `${process.env.REACT_APP_API_URL}/auth/login`,
-            data: {
-                id: email,
-                password: pwd,
-            },
-            withCredentials: true,
-        })
-        .then((res) => {
-            console.log(res.data);
-            if(res.data.errorMsg === 14 || res.data.errorMsg == 15) {
-                alert('잘못된 비밀번호이거나 존재하지 않는 사용자입니다.');
-                history.push("/login");
-                return;
-            }
-
-            //리프레시 토큰과 생사를 같이 해야 하므로 쿠키에 저장
-            authorizedUser.email = res.data.user.id;
-            authorizedUser.nickname = res.data.user.nickname;
-            // toUTCString() 으로 변환해야 함
-            const currentTime = new Date().getTime();
-            const targetTime = currentTime + res.data.refreshOption.maxAge;
-            const expireTime = new Date(targetTime);
-
-            const sessionStorage = new SessionStorageAPI();
-
-            //seesion에 토큰을 저장해도 되는가?
-            //쿠키 저장 : setCookies("userNickname", authorizedUser.nickname, "/", expireTime.toUTCString());
-            sessionStorage.setItem("userNickname", authorizedUser.nickname);
-            saveUserInfo(authorizedUser.nickname);
-            
-            sessionStorage.setItem("userId", authorizedUser.email);
-
-            history.push("/");
-        });
+        if(email === ""){
+            setErrorInfo("아이디를 입력해주세요.");
+        }
+        else if(pwd === ""){
+            setErrorInfo("비밀번호를 입력해주세요.");
+        }
+        else{
+            axios({
+                method: "POST",
+                url: `${process.env.REACT_APP_API_URL}/auth/login`,
+                data: {
+                    id: email,
+                    password: pwd,
+                },
+                withCredentials: true,
+            })
+            .then((res) => {
+                console.log(res.data);
+                if(res.data.errorMsg === 14 || res.data.errorMsg == 15) {
+                    alert('잘못된 비밀번호이거나 존재하지 않는 사용자입니다.');
+                    history.push("/login");
+                    return;
+                }
+    
+                //리프레시 토큰과 생사를 같이 해야 하므로 쿠키에 저장
+                authorizedUser.email = res.data.user.id;
+                authorizedUser.nickname = res.data.user.nickname;
+                // toUTCString() 으로 변환해야 함
+                const currentTime = new Date().getTime();
+                const targetTime = currentTime + res.data.refreshOption.maxAge;
+                const expireTime = new Date(targetTime);
+    
+                const sessionStorage = new SessionStorageAPI();
+    
+                //seesion에 토큰을 저장해도 되는가?
+                //쿠키 저장 : setCookies("userNickname", authorizedUser.nickname, "/", expireTime.toUTCString());
+                sessionStorage.setItem("userNickname", authorizedUser.nickname);
+                saveUserInfo(authorizedUser.nickname);
+                
+                sessionStorage.setItem("userId", authorizedUser.email);
+    
+                history.push("/");
+            });
+        }
     }
 
     function googleLogin(e) {
         e.preventDefault();
         window.location.href = `${process.env.REACT_APP_API_URL}/auth/googleAuth`;
+    }
+
+    function handleEnter(e){
+        if(e.keyCode === 13){ //javascript에서는 13이 enter키를 의미함
+            login();
+        }
     }
 
     return(
@@ -95,15 +115,17 @@ export const LoginRoute: React.FC = () => {
                 
                     <div className="grid-1 font-game-thick">로그인</div>
                     <div className="grid-2 ">
-                        <input type="email" placeholder="계정 이메일" required onChange={onChangeEmail}/>
-                        <input type="password" placeholder="비밀번호" required onChange={onChangePwd}/>
-                        
+                        <form action="">
+                            <input type="email" placeholder="계정 이메일" required onChange={onChangeEmail} onKeyDown={handleEnter}/>
+                            <input type="password" placeholder="비밀번호" required onChange={onChangePwd} onKeyDown={handleEnter} />              
+                            <div className='errorInfo'>{errorInfo}</div>
+                        </form>
                         <form className='google-login-form'>
-                                <button className="google-login" onClick={googleLogin}> <img src={googleLogo} />구글 로그인</button>
+                                <button disabled className="google-login" onClick={googleLogin}> <img src={googleLogo} />구글 로그인</button>
                         </form>
 
                         <div className="autoLogin-container">
-                            <input type="checkbox" id="autoLogin" name="autoLogin" />
+                            <input disabled type="checkbox" id="autoLogin" name="autoLogin" />
                             <label className="font-game-thin" htmlFor="autoLogin">로그인 상태 유지</label>
                         </div>
                     </div>
@@ -113,9 +135,9 @@ export const LoginRoute: React.FC = () => {
                         </button>
                     </div>
                     <div className="grid-4 font-game-thin">
-                        <a href="">아이디/비밀번호 찾기</a>
+                        <Link to="">아이디/비밀번호 찾기</Link>
                         <br />
-                        <a href="/#/signup">회원가입</a>
+                        <Link to="/signup">회원가입</Link>
                     </div>
                     
                 </div>
@@ -124,10 +146,10 @@ export const LoginRoute: React.FC = () => {
             </main>
             <footer className="login-footer font-game-thin">
                 <div className="footer-link-container">
-                    <a href="/">홈</a>
-                    <a href="">고객센터</a>
-                    <a href="">쿠키설정</a>
-                    <a href="">언어</a>
+                    <Link to="/">홈</Link>
+                    <Link to="/board/suggestion/1">고객센터</Link>
+                    <Link to="">쿠키설정</Link>
+                    <Link to="">언어</Link>
                 </div>
             </footer>
         </body>
